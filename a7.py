ddd5b6e5de721cfa782735093c6e82fafc724e0b
Logan-Magana <lgmagana@cps.edu>
2026-04-10 11:30:20 -0500

    3.5rd

--- a7.py
+++ a7.py
@@ -67,14 +67,14 @@ class BayesClassifier:
         # enumerate function, which loops over something and has an automatic counter.
         # write something like this to track progress (note the `# type: ignore` comment
         # which tells mypy we know better and it shouldn't complain at us on this line):
-        # for index, filename in enumerate(files, 1): # type: ignore
-        #     print(f"Training on file {index} of {len(files)}")
-        # #     <the rest of your code for updating frequencies here>
-        #     print(f"{index}: {filename}")
-        #     text = self.load_file(os.path.join(self.training_data_directory, filename))
-        #     print(text)
-        #     tokens= self.tokenize(text)
-        #     print(tokens)
+        for index, filename in enumerate(files, 1): # type: ignore
+            print(f"Training on file {index} of {len(files)}")
+        #     <the rest of your code for updating frequencies here>
+            print(f"{index}: {filename}")
+            text = self.load_file(os.path.join(self.training_data_directory, filename))
+            print(text)
+            tokens= self.tokenize(text)
+            print(tokens)
         # we want to fill pos_freqs and neg_freqs with the correct counts of words from
         # their respective reviews
         
@@ -87,16 +87,17 @@ class BayesClassifier:
         # test your code with neutral reviews)
             if filename.startswith(self.pos_file_prefix):
                 self.update_dict(tokens,self.pos_freqs)
+                self.save_dict(self.pos_freqs, self.pos_filename)
             elif filename.startswith(self.neg_file_prefix):
                 self.update_dict(tokens, self.neg_freqs)
-        print(self.pos_freqs)
+                self.save_dict(self.neg_freqs, self.neg_filename)
 
         # Updating frequences: to update the frequencies for each file, you need to get
         # the text of the file, tokenize it, then update the appropriate dictionary for
         # those tokens. We've asked you to write a function `update_dict` that will make
         # your life easier here. Write that function first then pass it your list of
         # tokens from the file and the appropriate dictionary
-        
+
 
         # for debugging purposes, it might be useful to print out the tokens and their
         # frequencies for both the positive and negative dictionaries
